9867515edd9345c28710422178317d9ca9cff292
Shane Gadsby <schme16@gmail.com>
2014-06-22 20:38:43 +1000

    Adding nodejs info
    Signed-off-by: Shane Gadsby <schme16@gmail.com>

---
+++ tests.js
@@ -0,0 +1,48 @@
+/*global localStorage:true*/
+
+
+
+
+
+/*Set up some environmentals*/
+
+//Assert
+assert = require('basic-assert');
+require('basic-assert');
+w = require('./jsCommons.js');
+
+//LocalStorage
+if (typeof localStorage === "undefined" || localStorage === null) {
+	var LocalStorage = require('node-localstorage').LocalStorage;
+	localStorage = new LocalStorage('./scratch');
+}
+
+
+//Tests that it is actually returning the correct time
+assert.is(w.getEpoch(), parseInt((new Date().getTime() / 1000).toFixed(0, 0), 0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
